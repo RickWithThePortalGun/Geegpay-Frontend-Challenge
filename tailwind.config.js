f268d59a884de41494403a0882cc11d5f156d4b7
Oyeniyi Victor <flamezy36@gmail.com>
2024-01-25 09:55:57 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        'xs': '412px',
       },
+      colors: {
+        typography: "#26282C",
+    },
     },
   },
   plugins: [],
+  darkMode:"class"
 };
