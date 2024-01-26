aecd42d9dd2337728e931942cd490aa3209ba346
Oyeniyi Victor <flamezy36@gmail.com>
2024-01-26 09:19:51 +0100

    final implementation of dark mode

--- app/provider.js
+++ app/provider.js
@@ -5,25 +5,20 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const Provider = ({ children }) => {
-  const [mounted, setMounted] = useState(false); // Corrected destructuring for useState
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     setMounted(true);
 
-   // Read system's theme preference
 const systemDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-// Check if a theme is already stored in localStorage
 const storedTheme = localStorage.getItem("theme");
 
-// Debugging logs
 console.log("System Dark Theme:", systemDarkTheme);
 console.log("Stored Theme:", storedTheme);
 
-// Use systemDarkTheme if no theme is stored, otherwise use the stored theme
 const darkTheme = storedTheme === "dark" || (!storedTheme && systemDarkTheme);
 
-// Debugging log
 console.log("Selected Theme:", darkTheme ? "dark" : "light");
 
 if (darkTheme) {
